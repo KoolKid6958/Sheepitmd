8d85daf5b8dbb54c0d1292da5e3995a4a41634da
KoolKid6958 <156125488+KoolKid6958@users.noreply.github.com>
2025-08-19 14:15:04 -0400

    feat: rename --genconfig to gen-config, and add a --path option

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
 /// CLI Manager for Sheepit.
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-
-struct Args {
-    /// Generate a default config based on the hardware in the current system.
-    #[arg(long)]
-    genconfig: bool,
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Generate a default config based on the hardware in the current system.
+    #[command(name = "gen-config")]
+    GenConfig {
+        /// Path to save the config in
+        #[arg(
+            short,
+            long,
+            value_name = "FILE",
+            default_value = "./.sheepit-manager.toml"
+        )]
+        path: PathBuf,
+    },
+}
+
 fn config() {
     println!("Config");
 }
+
 fn main() {
-    //Inital arg stuff
-    let args = Args::parse();
-    if args.genconfig {
-        config();
+    // Inital arg stuff.
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::GenConfig { path }) => {
+            println!("Config path: {:?}", path);
+            config();
+        }
+        None => {}
     }
 }
