8fa75547429b759589c59702d514802f9ef89d76
KoolKid6958 <156125488+KoolKid6958@users.noreply.github.com>
2025-08-18 21:03:52 -0400

    feat: add start for --genconfig arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+/// CLI Manager for Sheepit.
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+
+struct Args {
+    /// Generate a default config based on the hardware in the current system.
+    #[arg(long)]
+    genconfig: bool,
+}
+fn config() {
+    println!("Config");
+}
 fn main() {
-    println!("Hello, world!");
+    //Inital arg stuff
+    let args = Args::parse();
+    if args.genconfig {
+        config();
+    }
 }
