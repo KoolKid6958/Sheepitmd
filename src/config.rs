21777cabf97a2d6149f5e3987e50550d0cc1cb35
KoolKid6958 <156125488+KoolKid6958@users.noreply.github.com>
2025-08-21 12:39:54 -0400

    feat(gen-config): add config options for specific cpus & gpus

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,13 @@
 use serde::Serialize;
-use std::{fs, path::PathBuf};
+use std::{collections::BTreeMap, fs, path::PathBuf};
 
 #[derive(Serialize)]
 struct Config {
     general: General,
     paths: Paths,
     defaults: Defaults,
+    cpu: Cpu,
+    gpu: BTreeMap<String, Gpu>,
 }
 
 #[derive(Serialize)]
@@ -30,7 +32,24 @@ struct Defaults {
     cores: u16,
 }
 
+#[derive(Serialize)]
+struct Cpu {
+    ram: u16,
+    cores: u16,
+}
+
+#[derive(Serialize)]
+struct Gpu {
+    ram: u16,
+    cores: u16,
+}
+
 pub fn generate_config(path: PathBuf) {
+    // Dynamic GPU Map *CURRENTLY USES FIXED VALUES*
+    let mut gpu_map = BTreeMap::new();
+    gpu_map.insert("3080ti".to_string(), Gpu { ram: 0, cores: 0 });
+    gpu_map.insert("5060ti".to_string(), Gpu { ram: 0, cores: 0 });
+    // Write to the file
     let config = Config {
         general: General {
             client_name: "".to_string(),
@@ -45,6 +64,8 @@ pub fn generate_config(path: PathBuf) {
             sheepit_client_location: "/tmp/sheepit-client.jar".into(),
         },
         defaults: Defaults { ram: 0, cores: 0 },
+        cpu: Cpu { ram: 0, cores: 0 },
+        gpu: gpu_map,
     };
     let toml = toml::to_string(&config).unwrap();
     fs::write(path, toml).expect("Failed to generate config.");
