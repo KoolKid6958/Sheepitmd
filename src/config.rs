c301280feb9426faf0abc15b0f99e0193f1523d6
KoolKid6958 <156125488+KoolKid6958@users.noreply.github.com>
2025-08-21 13:36:13 -0400

    fix(gen-config): gpus are now added dynamically

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use nvml_wrapper::{Nvml, error::NvmlError};
 use serde::Serialize;
 use std::{collections::BTreeMap, fs, path::PathBuf};
 
@@ -7,6 +8,7 @@ struct Config {
     paths: Paths,
     defaults: Defaults,
     cpu: Cpu,
+    #[serde(skip_serializing_if = "BTreeMap::is_empty")]
     gpu: BTreeMap<String, Gpu>,
 }
 
@@ -42,13 +44,49 @@ struct Cpu {
 struct Gpu {
     ram: u16,
     cores: u16,
+    optix_id: String,
+}
+
+fn shorten_gpu_name(gpu_name: &str) -> String {
+    gpu_name
+        .replace("NVIDIA GeForce RTX", "")
+        .replace("NVIDIA GeForce GTX", "")
+        .replace(" ", "")
+        .to_lowercase()
+}
+
+fn get_gpus() -> Result<Vec<String>, NvmlError> {
+    let nvml = Nvml::init()?;
+    let num_gpu = nvml.device_count()?;
+    let mut gpus = Vec::new();
+
+    for i in 0..num_gpu {
+        let gpu = nvml.device_by_index(i)?;
+        let gpu_name = gpu.name()?;
+        let short_name = shorten_gpu_name(&gpu_name);
+        gpus.push(short_name);
+    }
+    Ok(gpus)
 }
 
 pub fn generate_config(path: PathBuf) {
-    // Dynamic GPU Map *CURRENTLY USES FIXED VALUES*
+    // Dynamic GPU Map
     let mut gpu_map = BTreeMap::new();
-    gpu_map.insert("3080ti".to_string(), Gpu { ram: 0, cores: 0 });
-    gpu_map.insert("5060ti".to_string(), Gpu { ram: 0, cores: 0 });
+    if let Ok(gpus) = get_gpus() {
+        let mut i = 0;
+        for gpu_name in gpus {
+            let optix_id = format!("OPTIX_{}", i);
+            i = i + 1;
+            gpu_map.insert(
+                gpu_name,
+                Gpu {
+                    ram: 0,
+                    cores: 0,
+                    optix_id: optix_id,
+                },
+            );
+        }
+    }
     // Write to the file
     let config = Config {
         general: General {
