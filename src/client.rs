699ae683cfaf0c4fe1a0580c0facdc6519ff760a
KoolKid6958 <156125488+KoolKid6958@users.noreply.github.com>
2025-11-01 22:15:31 -0400

    feat: change download logic to reqwest

--- src/client.rs
+++ src/client.rs
@@ -1,22 +1,21 @@
 // This file is meant for managing the sheepit client itself.
 use crate::config;
 use crate::config::Config;
-use std::{
-    fs,
-    path::{Path, PathBuf},
-    process::Command,
-};
+use reqwest::Client;
+use std::path::{Path, PathBuf};
+use tokio::fs;
 
-pub fn start_client(client: &str, config_path: PathBuf) {
+pub async fn start_client(client: &str) {
+    let config_path: PathBuf = "./.sheepit-manager.toml".into();
     let config = config::read_config(config_path.clone());
     println!("Starting: {}", client);
     // CURRENTLY THIS CODE DOESNT USE A SPECIFIC CLIENT. IT ONLY STARTS IT ON CPU WITH DEFAULT SETTINGS.
     let client_path: PathBuf = config.paths.sheepit_client_location.clone();
     match check_if_client_exists(client_path) {
         true => {}
-        false => {
-            download_client(config.clone()).expect("There was an error downloading the client")
-        }
+        false => download_client(config.clone())
+            .await
+            .expect("There was an error downloading the client"),
     }
 }
 
@@ -29,7 +28,7 @@ fn check_if_client_exists(path: PathBuf) -> bool {
     }
 }
 
-fn download_client(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+async fn download_client(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     println!("Downloading client");
     let url = "https://www.sheepit-renderfarm.com/media/applet/client-latest.php";
     let client_location = config.paths.sheepit_client_location;
@@ -39,17 +38,19 @@ fn download_client(config: Config) -> Result<(), Box<dyn std::error::Error>> {
 
     if !Path::new(&path).exists() {
         println!("Directory {:?} doesnt exist. Creating now.", path);
-        fs::create_dir_all(path).expect(
+        fs::create_dir_all(path).await.expect(
             "Failed to create directory. Please check that you have the necessary permissions",
         );
     } else {
     }
-    let status = Command::new("curl")
-        .args(["-#", "-L", "-o", client_location.to_str().unwrap(), url])
-        .status()?;
-    if !status.success() {
-        return Err(format!("curl failed with exit code {:?}", status.code()).into());
+    let client = Client::new();
+    let response = client.get(url).send().await?;
+    if !response.status().is_success() {
+        return Err(format!("Download failed: {}", response.status()).into());
     }
+    let bytes = response.bytes().await?;
+    fs::write(client_location, &bytes).await?;
+
     println!("Client Downloaded!");
     Ok(())
 }
